bda40713da03089a45aa40eeff1d5b50264e45a0
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-01-13 14:04:17 +0000

    working on edit form

--- app.py
+++ app.py
@@ -158,6 +158,15 @@ def add_recipe():
         "add_recipe.html", categories=categories, marks=marks)
 
 
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    marks = mongo.db.marks.find().sort("mark", 1)
+    return render_template(
+        "edit_recipe.html", recipe=recipe, categories=categories, marks=marks)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
