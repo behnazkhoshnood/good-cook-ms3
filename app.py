99f8035fc3a10d80e56d75ff737a257ebd95bea3
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-02-09 12:21:24 +0000

    changing the debug to False

--- app.py
+++ app.py
@@ -565,4 +565,4 @@ def delete_mark(mark_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
